42b9b7955dd65f6ec2c87c8144241d864f4f1e03
azunic <zunicana13@gmail.com>
2021-02-23 01:46:38 +0100

    style in profile component

--- client/src/components/profile/ProfileComponent.js
+++ client/src/components/profile/ProfileComponent.js
@@ -1,9 +1,10 @@
 import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
+import { Input, InputNumber } from 'antd';
+import { Button, Accordion, Card, Form } from 'react-bootstrap';
+import ListGroup from 'react-bootstrap/ListGroup';
 
-import { Form, Input, InputNumber } from 'antd';
-import { Button, Accordion, Card } from 'react-bootstrap';
 export default function ProfileComponent(props) {
   console.log(props);
   console.log(props.props);
@@ -26,40 +27,39 @@ export default function ProfileComponent(props) {
         <div className="security-details-head">
           <h2>Moj profil</h2>
         </div>
-        <Form>
-          <Card>
-            <span>Username:</span>
-            {profile && `${profile.username}`}
-            <br></br>
-          </Card>
-          <Card>
-            <span>First name:</span>
-            {profile && `${profile.firstName}`}
-            <br></br>
-          </Card>
-          <Card>
-            <span>Last name:</span>
-            {profile && `${profile.firstName}`}
-            <br></br>
-          </Card>
-          <Card>
-            <span>E-Mail:</span>
-            {profile && `${profile.email}`}
-            <br></br>
-          </Card>
-          <Card>
-            <span>Broj proizvoda na wishlist:</span>
-            {profile && `${profile.wishListedProducts.length}`}
-            <Link>Idi na wishlistu</Link>
-            <br></br>
-          </Card>
-          <Card>
-            <span>Moji proizvodi:</span>
-            {profile && `${profile.ownedProducts.length}`}
-            <Link>Idi na moje proizvode</Link>
-            <br></br>
-          </Card>
-        </Form>
+        <Form.Group>
+          <Form.Label>Username:</Form.Label>
+          <ListGroup.Item disabled>{profile && `${profile.username}`}</ListGroup.Item>
+        </Form.Group>
+
+        <Form.Group>
+          <Form.Label>First name:</Form.Label>
+          <ListGroup.Item disabled> {profile && `${profile.firstName}`}</ListGroup.Item>
+        </Form.Group>
+
+        <Form.Group>
+          <Form.Label>Last Name:</Form.Label>
+          <ListGroup.Item disabled> {profile && `${profile.lastName}`}</ListGroup.Item>
+        </Form.Group>
+
+        <Form.Group>
+          <Form.Label>E-mail</Form.Label>
+          <ListGroup.Item disabled> {profile && `${profile.email}`}</ListGroup.Item>
+        </Form.Group>
+
+        <Form.Group>
+          <Form.Label> Broj proizvoda na wish listi</Form.Label>
+          <ListGroup.Item disabled>{profile && `${profile.wishListedProducts.length}`}</ListGroup.Item>
+          <Link>Idi na wishlistu</Link>
+        </Form.Group>
+
+        <Form.Group>
+          <Form.Label>Moji proizvodi</Form.Label>
+          <ListGroup.Item disabled> {profile && `${profile.ownedProducts.length}`}</ListGroup.Item>
+          <Link>Idi na moje proizvode</Link>
+        </Form.Group>
+
+        <br></br>
       </div>
     </div>
   );
